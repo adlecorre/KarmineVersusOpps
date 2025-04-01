804cb8c860b2c1ee4ac7abd51d211975ac5bd2aa
Adrien Le Corre <94919916+adlecorre@users.noreply.github.com>
2025-04-01 19:03:54 +0200

    Optimisation tri des matchs. Création du fichier de configuration des tournois LoL.

--- src/app/league-of-legends.service.ts
+++ src/app/league-of-legends.service.ts
@@ -2,22 +2,57 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LeagueOfLegendsService {
-  private apiUrl = "https://esports-api.lolesports.com/persisted/gw/getStandings?hl=fr-FR&tournamentId=112445505684034122";
-  private key = "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z";
-  private tournamentKey = "112445505684034122";
+  private apiUrl =
+    'https://esports-api.lolesports.com/persisted/gw/getStandings?hl=fr-FR&tournamentId=112445505684034122';
+  private key = '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z';
+  private tournamentKey = '112445505684034122';
   private lang = 'fr-FR';
+  private matches: any[] = [];
 
   constructor(private http: HttpClient) {}
 
-  getMatches(): Observable<any> {
+  getAllMatches(): Observable<any> {
     const headers = new HttpHeaders({ 'x-api-key': this.key });
-    let httpParams = new HttpParams().set('hl', this.lang).set('tournamentId', this.tournamentKey);
-    
+    let httpParams = new HttpParams()
+      .set('hl', this.lang)
+      .set('tournamentId', this.tournamentKey);
+
     return this.http.get(this.apiUrl, { headers });
+  }
+
+  getMatches(): Observable<any[]> {
+    return new Observable((observer) => {
+      this.getAllMatches().subscribe({
+        next: (data) => {
+          const extractedMatches = this.extractMatchesKC(data);
+          observer.next(extractedMatches);
+          observer.complete();
+        },
+        error: (err) => observer.error(err),
+      });
+    });
+  }
+
+  extractMatchesKC(data: any): any[] {
+    const matches: any[] = [];
+    data.data.standings.forEach((standing: any) => {
+      standing.stages.forEach((stage: any) => {
+        stage.sections.forEach((section: any) => {
+          section.matches.forEach((match: any) => {
+            if (match.teams.length === 2) {
+              const teamCodes = match.teams.map((team: any) => team.code);
+              if (teamCodes.includes('KCB') || teamCodes.includes('KC')) {
+                matches.push(match);
+              }
+            }
+          });
+        });
+      });
+    });
+    return matches;
   }
 }
--- src/app/opps-page/opps-page.component.ts
+++ src/app/opps-page/opps-page.component.ts
@@ -1,32 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { oppsConfig } from '../../config/opps.config'
+import { oppsConfig } from '../../config/opps.config';
 import { LeagueOfLegendsService } from '../league-of-legends.service';
-
 
 @Component({
   selector: 'app-opps-page',
   templateUrl: './opps-page.component.html',
-  styleUrls: ['./opps-page.component.css']
+  styleUrls: ['./opps-page.component.css'],
 })
 export class OppsPageComponent implements OnInit {
   oppsName: string | undefined;
-  opps = oppsConfig
+  opps = oppsConfig;
   opp: any = {};
   matches: any[] = [];
   teams: any[] = [];
 
   constructor(
     private readonly route: ActivatedRoute,
-    private lolMatchService: LeagueOfLegendsService) {}
+    private readonly lolMatchService: LeagueOfLegendsService
+  ) {}
 
   ngOnInit(): void {
-      this.route.paramMap.subscribe(params => {
-      this.oppsName = params.get('oppsName') ?? 'Not an opp';
-      this.getStandings();
-      this.getOppDetails();
+  this.route.paramMap.subscribe((params) => {
+    this.oppsName = params.get('oppsName') ?? 'Not an opp';
+    this.getOppDetails();
+
+    this.lolMatchService.getMatches().subscribe((data) => {
+      this.extractTeams(data);
+    });
   });
-  }
+}
 
   /**
    * Définit Opp comme l'objet de configuration sachant le nom
@@ -39,38 +42,14 @@ export class OppsPageComponent implements OnInit {
     }
   }
 
-
-  getStandings(): void {
-    this.lolMatchService.getMatches().subscribe({
-      next: (data) => {
-        this.extractTeams(data);
-      },
-      error: (err) => console.error('Erreur lors de la récupération des standings:', err)
+  extractTeams(matches: any[]): void {
+    matches.forEach((match: any) => {
+      if (match.teams.length === 2) {
+        const teamCodes = match.teams.map((team: any) => team.code);
+        if (this.opp.aliases.some((alias: string) => teamCodes.includes(alias))) {
+          this.matches.push(match);
+        }
+      }
     });
   }
-
-extractTeams(data: any): any[] {
-    data.data.standings.forEach((standing: any) => {
-        standing.stages.forEach((stage: any) => {
-            stage.sections.forEach((section: any) => {
-                section.matches.forEach((match: any) => {
-                   if (match.teams.length === 2) {
-                        const teamCodes = match.teams.map((team: any) => team.code);
-                        if ((teamCodes.includes("KCB") || teamCodes.includes("KC")) 
-                        && this.opp.aliases.some((alias: string) => teamCodes.includes(alias))) {
-                            this.matches.push(match);
-                        }
-                    } 
-                });
-            });
-        });
-    });
-    return this.matches;
-}
-
-
-
-
-
-
 }
